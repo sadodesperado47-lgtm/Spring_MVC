0b5de47626d94010430b290f258ff135391d0cb6
Andrey T. <troshin_a_a@mail.ru>
2026-04-19 15:05:11 +0300

    new_attempt_addUser,remove,edit

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -18,20 +18,36 @@ public class UserController {
         this.userService = userService;
     }
 
+    // Вывод всей таблицы (Edit userId — чтобы знать, кого редактировать)
     @GetMapping
-    public String getUsers(Model model) {
+    public String getUsers(Model model, @RequestParam(value="editId", required=false) Long editId) {
         model.addAttribute("users", userService.getAllUsers());
-        return "users"; // имя шаблона (users.html)
+        if (editId != null) {
+            User userToEdit = userService.getUserById(editId);
+            model.addAttribute("editUser", userToEdit);
+            model.addAttribute("editId", editId);
+        }
+        return "users";
     }
 
+    // Добавление пользователя
     @PostMapping
     public String addUser(@ModelAttribute("user") User user) {
         userService.addUser(user);
         return "redirect:/users";
     }
+
+    // Удаление пользователя
     @GetMapping("/delete/{id}")
     public String removeUser(@PathVariable Long id) {
         userService.removeUser(id);
+        return "redirect:/users";
+    }
+
+    // Обновление пользователя (Update)
+    @PostMapping("/edit")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.updateUser(user);
         return "redirect:/users";
     }
 }
